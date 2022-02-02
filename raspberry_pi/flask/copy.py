1137e76a9042b49b305d356dcd684e47dd45888b
Christopher Ryu <cryu17@gmail.com>
2022-02-02 15:04:04 -0800

    Cleanup

--- raspberry_pi/flask/copy.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, make_response
-
-app = Flask(__name__)
-
-@app.route('/<path:request>')
-def handle_request(request):
-    response = make_response(str(request), 200)
-    response.mimetype = "text/plain"
-    return response
